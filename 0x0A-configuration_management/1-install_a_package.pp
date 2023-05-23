cadf9b56e51eaff022b91602ce155cb5bb4b0391
hkiriama <humphrey.kiriama@gmail.com>
2023-05-23 17:19:03 +0000

    fix

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,14 +1,5 @@
-# Using Puppet, install puppet-lint.
-
-#package { 'puppet-lint':
-#    ensure   => '2.5.0',
-#    name     => 'puppet-lint',
-#    provider => 'gem',
-#    source   => 'http://rubygems.org',
-#}
-
-package { 'flask':
-    ensure   => '2.1.0',
-    name     => 'flask',
-    provider => 'pip3',,
+#Puppet file to install puppet-lint
+package { 'puppet-lint':
+  ensure   => '2.1.1',
+  provider => 'gem',
 }
